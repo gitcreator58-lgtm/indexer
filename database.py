21d778e3317afa13710707376a3e87169d81a446
gitcreator58-lgtm <gitcreator58@gmail.com>
2026-01-22 19:39:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 import motor.motor_asyncio
 from config import Config
 
+# --- Database Connection ---
 client = motor.motor_asyncio.AsyncIOMotorClient(Config.DB_URL)
 db = client['MyTelegramBot']
-files_col = db['files']
 
+# --- Collections ---
+files_col = db['files']
+users_col = db['users']
+
+# --- File Indexing Functions ---
 async def add_file(msg):
     # Only index video or documents
     media = msg.video or msg.document
@@ -18,12 +23,22 @@ async def add_file(msg):
         'msg_id': msg.id,
         'chat_id': Config.CHANNEL_ID
     }
-    # Avoid duplicates
+    # Avoid duplicates: Update if exists, Insert if new
     await files_col.update_one({'file_id': media.file_id}, {'$set': file_det}, upsert=True)
 
 async def search_files(query):
-    # Regex search for the movie name
+    # Regex search for the movie name (Case insensitive)
     return files_col.find({'file_name': {'$regex': query, '$options': 'i'}})
 
 async def get_file_by_id(file_id):
     return await files_col.find_one({'file_id': file_id})
+
+# --- User Management Functions (Required for Broadcast) ---
+async def add_user(user_id):
+    # Save user to DB if they don't exist
+    user_data = {'user_id': user_id}
+    await users_col.update_one({'user_id': user_id}, {'$set': user_data}, upsert=True)
+
+async def get_all_users():
+    # Return a cursor to iterate over all users
+    return users_col.find({})
